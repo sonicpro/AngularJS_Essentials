e4e7749af24e830e877adddd945440dd2b34f2a6
sonicpro <sonicpro@ukrpost.ua>
2019-07-18 15:47:20 +0300

    $httpBackend service added to runtime configuration.

---
+++ Ch4/run.js
@@ -0,0 +1,12 @@
+/*global app */
+
+app.run(function ($rootScope, $window) {
+  $rootScope.$on("$routeChangeError", function(event, current, previous, rejection) {
+    $window.location.href = "error.html";
+  });
+});
+
+app.run(function ($httpBackend) {
+  $httpBackend.whenGET("/cars").respond([{plate: "AAA9999", color: "Blue", entrance: new Date()}, {plate: "AAA9988", color: "Blue", entrance: new Date()}]);
+  $httpBackend.whenGET(/./).passThrough();
+});
